ed349a728fc05f25f52de4981a4b6f4cdb4a6d14
Matt Carlson and Rony Nasr <rony@>
2016-06-30 16:05:17 -0700

    get app passing integration specs

--- spec/anagrams_integration_spec.rb
+++ spec/anagrams_integration_spec.rb
@@ -1,1 +1,14 @@
-require('capybar')
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the anagram path', {:type => :feature}) do
+  it('takes two strings and determines which words from second string are anagrams of the first') do
+    visit('/')
+    fill_in('input_word', :with => 'cat')
+    fill_in('word_list', :with => 'tac, act, bob')
+    click_button('Go')
+    expect(page).to have_content("tac, act")
+  end
+end
